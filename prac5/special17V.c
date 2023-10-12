4f58776cf0764cadc16e6821799bf3316825d61a
eduardkarpow <edkarpov10@gmail.com>
2023-10-12 13:07:17 +0300

    prac5 commit4

--- prac5/special17V.c
+++ prac5/special17V.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void count(int* array, int N) {
-    int countPositive = 0;
-    int sumNegative = 0;
+void count(int* array, int N, int *countPositive, int *sumNegative) {
     for(int i = 0; i < N; i++){
 	if(array[i] > 0){
-	    countPositive++;
+	    *countPositive+=1;
 	} else if(array[i] < 0){
-	    sumNegative += array[i];
+	    *sumNegative += array[i];
 	}
     }    
-    printf("Count positive: %d\nSum negative: %d\n", countPositive, sumNegative);
 }
 int main(){
     int N;
@@ -20,7 +17,13 @@ int main(){
     for(int i = 0; i < N; i++){
 	scanf("%d\n",&array[i]);
     }
-    count(array, N);
+    int countPositive = 0;
+    int sumNegative = 0;
+    count(array, N, &countPositive, &sumNegative);
+    printf("Count positive: %d\nSum negative: %d\n", countPositive, sumNegative);
+    void (*func) (int*, int, int*, int*);
+    func = count;
+    printf("Pointer to function: %p\n", func);
     free(array);
 
     return 0;
